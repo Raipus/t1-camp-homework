99d5887a2fcc437a1484f50b617a6612d2b235d3
Ivanov Nikita <nikita18.907@gmail.com>
2025-07-20 23:43:33 +0300

    feat: add real task visibility
    Add real task visibility.

--- src/widgets/task-status-list/ui/task-status-list.tsx
+++ src/widgets/task-status-list/ui/task-status-list.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Divider, T } from '@admiral-ds/react-ui';
+import { Divider, IconButton, T, TooltipHoc } from '@admiral-ds/react-ui';
 import { TaskItem } from '@/features/task-item';
+import { type ITask } from '@/entities/task';
+import PlusCircleSolid from '@admiral-ds/icons/build/service/PlusCircleSolid.svg?react';
+import { Link, useLocation } from 'react-router-dom';
 
 type Props = {
-  tasks?: Array<Number>;
+  tasks: ITask[];
   title: string;
   isMobile: boolean;
 };
@@ -34,28 +37,39 @@ const TaskList = styled.div<{ $isMobile: boolean }>`
   gap: 1rem;
   overflow-y: auto;
   width: 100%;
-  padding: 0.5rem 1.5rem;
-  scrollbar-width: none; /* Firefox */
-  -ms-overflow-style: none; /* IE/Edge */
+  padding: 1rem 1.5rem;
 
+  scrollbar-width: none;
+  -ms-overflow-style: none;
   &::-webkit-scrollbar {
-    display: none; /* Chrome, Safari */
+    display: none;
   }
 
   ${({ $isMobile }) => ($isMobile ? `box-sizing: border-box;` : ``)}
 `;
 
-export const TaskStatusList: React.FC<Props> = ({ title, isMobile }) => {
+const ButtonWithTooltip = TooltipHoc(IconButton);
+
+export const TaskStatusList: React.FC<Props> = ({ title, isMobile, tasks = [] }) => {
+  const location = useLocation();
+
   return (
     <StatusDiv $isMobile={isMobile}>
-      <T font="Header/H5" as="h2">
-        {title}
-      </T>
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+        <T font="Header/H5" as="h2">
+          {title}
+        </T>
+        <Link to={`/task/new`} state={{ backgroundLocation: location }}>
+          <ButtonWithTooltip renderContent={() => 'Создать задачу'} dimension="s">
+            <PlusCircleSolid />
+          </ButtonWithTooltip>
+        </Link>
+      </div>
       <Divider />
       <TaskList $isMobile={isMobile}>
-        {/* Временные задания */}
-        <TaskItem />
-        <TaskItem />
+        {tasks.map((task) => (
+          <TaskItem key={task.id} task={task} />
+        ))}
       </TaskList>
     </StatusDiv>
   );
