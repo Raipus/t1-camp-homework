f2c28e6862810d17279d4c61c41d61313348b240
Raipus <nikita18.907@gmail.com>
2025-07-25 15:44:53 +0300

    feat: selectors change
    BREAKING CHANGE: selectors change.

--- src/entities/task/model/selectors.ts
+++ src/entities/task/model/selectors.ts
@@ -1,5 +1,6 @@
 import type { RootState } from '@app/store';
 
 export const selectTasks = (state: RootState) => state.tasks.list;
-export const selectTaskById = (id: number) => (state: RootState) =>
-  state.tasks.list.find((task) => task.id === id);
+export const selectTaskById = (state: RootState) => state.tasks.currentTask;
+export const selectTasksLoading = (state: RootState) => state.tasks.loading;
+export const selectTasksError = (state: RootState) => state.tasks.error;
